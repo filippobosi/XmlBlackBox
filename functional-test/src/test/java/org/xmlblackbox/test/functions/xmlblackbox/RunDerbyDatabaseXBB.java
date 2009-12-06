ff9ad25c7dc16160e5772d4a37ed3b7db3ad5e12
acrea <acrea@5f806731-4c62-402e-b2e1-d8f9edf5552e>
2009-12-06 15:43:22 +0000

    inserimento di derbu
    git-svn-id: https://version.imolinfo.it/svn/XmlBlackBox/trunk@52 5f806731-4c62-402e-b2e1-d8f9edf5552e

---
+++ functional-test/src/test/java/org/xmlblackbox/test/functions/xmlblackbox/RunDerbyDatabaseXBB.java
@@ -0,0 +1,59 @@
+package org.xmlblackbox.test.functions.xmlblackbox;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.xmlblackbox.test.infrastructure.exception.RunPluginAbnormalTermination;
+import org.xmlblackbox.test.infrastructure.functions.GenericRunnablePlugin;
+import org.xmlblackbox.test.infrastructure.util.MemoryData;
+
+public class RunDerbyDatabaseXBB extends GenericRunnablePlugin  {
+
+    private final static Logger log = Logger.getLogger(RunDerbyDatabaseXBB.class);
+
+	@Override
+	public void execute(Properties prop, MemoryData memory) throws RunPluginAbnormalTermination {
+
+		String driver = (String)prop.get("driver");
+		log.info("driver "+driver);
+        try {
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
+            Properties props = new Properties(); // connection properties
+            props.put("user", "xmlblackbox");
+            props.put("password", "xmlblackbox");
+
+            Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/xmlblackbox;create=true", props);
+
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RunPluginAbnormalTermination();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RunPluginAbnormalTermination();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RunPluginAbnormalTermination();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RunPluginAbnormalTermination();
+		}
+
+    }
+
+	@Override
+	public List<String> getParametersName() {
+		ArrayList<String> ret = new ArrayList();
+		return ret;
+	}
+
+}
