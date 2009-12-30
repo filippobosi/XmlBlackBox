6d9c71844394326f9c66636530a40c95c4294033
acrea <acrea@5f806731-4c62-402e-b2e1-d8f9edf5552e>
2009-12-30 23:36:36 +0000

    git-svn-id: https://version.imolinfo.it/svn/XmlBlackBox/trunk@71 5f806731-4c62-402e-b2e1-d8f9edf5552e

--- functional-test/configRepository.bat
+++ functional-test/configRepository.bat
@@ -1,2 +1,4 @@
 call mvn install:install-file -DgroupId=saxon8 -DartifactId=saxon8-dom -Dversion=8.6.1 -Dpackaging=jar -Dfile=lib/saxon-8-6-1/saxon8-dom.jar -DgeneratePom=true -o
 call mvn install:install-file -DgroupId=saxon8 -DartifactId=saxon8 -Dversion=8.6.1 -Dpackaging=jar -Dfile=lib/saxon-8-6-1/saxon8.jar -DgeneratePom=true -o
+call mvn install:install-file -DgroupId=net.sf.saxon -DartifactId=saxon-dom -Dversion=9 -Dpackaging=jar -Dfile=lib/saxon-9-1-0-8/saxon9-dom.jar -DgeneratePom=true -o
+call mvn install:install-file -DgroupId=net.sf.saxon -DartifactId=saxon -Dversion=9 -Dpackaging=jar -Dfile=lib/saxon-9-1-0-8/saxon9.jar -DgeneratePom=true -o
