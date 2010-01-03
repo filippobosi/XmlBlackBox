613018b026cf1c784d8f8ba90a5e8de5499b8b43
acrea <acrea@5f806731-4c62-402e-b2e1-d8f9edf5552e>
2010-01-03 22:28:37 +0000

    webApplicationTest con config maven
    git-svn-id: https://version.imolinfo.it/svn/XmlBlackBox/trunk@91 5f806731-4c62-402e-b2e1-d8f9edf5552e

---
+++ WebApplicationTestMaven/src/main/java/org/xmlblackbox/test/webservice/NewWebService.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.xmlblackbox.test.webservice;
+
+import javax.jws.WebService;
+
+/**
+ *
+ * @author examar
+ */
+@WebService()
+public class NewWebService {
+
+    public Person[] getPerson(Person[] person){
+        int number =2;
+
+        
+        Person[] people = new Person[number];
+        people[0] = new Person();
+        people[0].setName("Mario");
+        people[0].setSurname("Rossi");
+        people[0].setAddress("Via romaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("WebApplicationExample Creata la persona Mario Rossi Via romaaaaaaaaaaaaaaaaaaaa");
+
+        people[1] = new Person();
+        people[1].setName("Paolo");
+        people[1].setSurname("Bianchi");
+        people[1].setAddress("Via milanooooooo");
+
+        System.out.println("WebApplicationExample Creata la persona Paolo Biianchi Via milanoooooo");
+
+        return people;
+    }
+
+}
---
+++ WebApplicationTestMaven/src/main/java/org/xmlblackbox/test/webservice/Person.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.xmlblackbox.test.webservice;
+
+/**
+ *
+ * @author examar
+ */
+public class Person {
+
+    private String name;
+    private String surname;
+    private String address;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the surname
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * @param surname the surname to set
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+}
